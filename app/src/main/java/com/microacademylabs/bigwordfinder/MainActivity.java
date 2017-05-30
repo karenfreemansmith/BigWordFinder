71dd959611f38c2318f029d7439b9be1c1306f7e
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-30 12:15:12 -0700

    add words to list

--- app/src/main/java/com/microacademylabs/bigwordfinder/MainActivity.java
+++ app/src/main/java/com/microacademylabs/bigwordfinder/MainActivity.java
@@ -2,17 +2,22 @@ package com.microacademylabs.bigwordfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
   private TextView hintText;
   private EditText guessWord;
   private Button okButton;
   private ListView wordList;
+  private ArrayList<String> guesses;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +28,21 @@ public class MainActivity extends AppCompatActivity {
     hintText.setText(Words.getWord());
 
     guessWord=(EditText) findViewById(R.id.editGuess);
+    guesses = new ArrayList<String>();
 
     wordList=(ListView) findViewById(R.id.wordList);
+
 
     okButton=(Button) findViewById(R.id.okButton);
     okButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         // add word guess to array-list view
+        guesses.add(guessWord.getText().toString());
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, guesses);
+        wordList.setAdapter(adapter);
+
       }
     });
   }
