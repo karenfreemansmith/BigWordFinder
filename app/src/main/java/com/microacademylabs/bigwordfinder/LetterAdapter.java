d9893823e3f02461643240f2027ab352074acc40
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-06-01 13:52:10 -0700

    refactor hint display as grid

---
+++ app/src/main/java/com/microacademylabs/bigwordfinder/LetterAdapter.java
@@ -0,0 +1,55 @@
+package com.microacademylabs.bigwordfinder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Karen Freeman-Smith on 6/1/2017.
+ */
+
+public class LetterAdapter extends BaseAdapter {
+  private Context mContext;
+  private String[] mLetters;
+
+  public LetterAdapter (Context context, String[] letters) {
+    this.mContext = context;
+    //Convert List to String[]?
+    this.mLetters = letters;
+  }
+
+  @Override
+  public int getCount() {
+    return mLetters.length;
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return null;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View gridView;
+    if(convertView==null) {
+      gridView = inflater.inflate(R.layout.hint_grid_item, null);
+      TextView letterView = (TextView) gridView.findViewById(R.id.grid_item_letter);
+      letterView.setText(mLetters[position]);
+   } else {
+      gridView = (View) convertView;
+    }
+    return gridView;
+  }
+
+}
--- app/src/main/java/com/microacademylabs/bigwordfinder/MainActivity.java
+++ app/src/main/java/com/microacademylabs/bigwordfinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.microacademylabs.bigwordfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,15 +8,21 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-  private TextView hintText;
+  //private TextView hintText;
+  private GridView letterGrid;
   private EditText guessWord;
   private Button okButton;
+  private Button scoreButton;
   private ListView wordList;
   private ArrayList<String> guesses;
 
@@ -24,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    hintText=(TextView)findViewById(R.id.hintText);
-    hintText.setText(Words.getHint());
+    letterGrid = (GridView) findViewById(R.id.hintGrid);
+    String[] hint = getHint(Words.getWord());
+    letterGrid.setAdapter(new LetterAdapter(this, hint));
+    //Toast.makeText(this, hint.toString(), Toast.LENGTH_SHORT).show();
 
     guessWord=(EditText) findViewById(R.id.editGuess);
     guesses = new ArrayList<String>();
@@ -37,14 +46,30 @@ public class MainActivity extends AppCompatActivity {
     okButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // add word guess to array-list view
         guesses.add(guessWord.getText().toString());
         guessWord.setText("");
 
         ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, guesses);
         wordList.setAdapter(adapter);
-
       }
     });
+
+    scoreButton=(Button)findViewById(R.id.btnScore);
+    scoreButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Show Score Here - or show score activity?", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(intent);
+      }
+    });
+  }
+
+  private String[] getHint(String word) {
+// String url = "https://en.oxforddictionaries.com/definition/" + word for definition
+
+    String[] hint = Arrays.asList(word.split("")).toArray(new String[0]);;
+    // Collections.shuffle(hint);
+    return hint;
   }
 }
