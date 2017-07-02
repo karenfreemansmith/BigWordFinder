2fcb71fb2c22594079951eff021ae5e911848816
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-07-02 16:47:10 -0700

    add contract class for words data

---
+++ app/src/main/java/com/microacademylabs/bigwordfinder/data/WordContract.java
@@ -0,0 +1,32 @@
+package com.microacademylabs.bigwordfinder.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Karen Freeman-Smith on 7/2/2017.
+ */
+
+public final class WordContract {
+
+  public static abstract class WordEntry implements BaseColumns {
+
+    public static final String TABLE_NAME = "words";
+    public static final String _ID = "_id";
+    public static final String COLUMN_WORD = "word";
+    public static final String COLUMN_DEFINITION = "definition";
+    public static final String COLUMN_CLASS = "class";
+    public static final String COLUMN_IMAGE = "image_name";
+
+    public static final int CLASS_12 = 12;
+    public static final int CLASS_11 = 11;
+    public static final int CLASS_10 = 10;
+    public static final int CLASS_9 = 9;
+    public static final int CLASS_8 = 8;
+    public static final int CLASS_7 = 7;
+    public static final int CLASS_6 = 6;
+    public static final int CLASS_5 = 5;
+    public static final int CLASS_4 = 4;
+    public static final int CLASS_3 = 3;
+
+  }
+}
